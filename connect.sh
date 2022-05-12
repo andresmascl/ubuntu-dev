ef25ae88fd56bd7e64900ded44cbef901939493f
amoreno.c <amoreno.c@d2iq.com>
2022-05-12 17:56:30 -0400

    removed some garbage

--- connect.sh
+++ connect.sh
@@ -6,8 +6,4 @@ cert_path=$(cat inventory | grep -Po "(?<==).*\.pem")
 #Get Host from inventory file
 host=$(cat inventory | grep -Po "(.*)amazonaws\.com")
 
-#ssh -i $cert_path -t ubuntu@$host "tmux new -s dev-session"
-
-#ssh -i $cert_path -t ubuntu@$host "tmux attach-session -t dev-session"
-
 ssh -i $cert_path -t ubuntu@$host 
--- main.sh
+++ main.sh
@@ -12,21 +12,15 @@ terraform apply -var owner="$AWS_OWNER"
 all_arguments=$@
 
 retrieve_credentials=false
-for i in $@
-do
-    if [ "$i" == 'local-setup' ]; then
-        echo '========== Local setup =============='
-        ./local-setup.sh
-    fi
-done
+
+echo '========== Local setup =============='
+./local-setup.sh
 
 for i in $@
 do
-    if [ "$i" == 'deploy-komdr' ]; then
-        echo '========== Local setup =============='
-        ./local-setup.sh
+    if [ "$i" == 'deploy-kommander' ]; then
 
-        echo '========== Deploying Komdr =========='
+        echo '========== Deploying Kommander =========='
         ./deploy-komdr.sh
 
         retrieve_credentials=true
@@ -35,20 +29,27 @@ done
 
 for i in $@
 do
-    if [ "$i" == 'install-kapt' ]; then
-        echo '========== Local setup =============='
-        ./local-setup.sh
+    if [ "$i" == 'install-kaptain' ]; then
 
-        echo '========== Deploying Komdr =========='
+        echo '========== Deploying Kommander =========='
         ./deploy-komdr.sh
         
-        echo '========== Installing Kapt ==========='
+        echo '========== Installing Kaptain ==========='
         ./install-kapt.sh
 
         retrieve_credentials=true
     fi
 done
 
+
+# output ssh connection command
+cert_path=$(cat inventory | grep -Po "(?<==).*\.pem")
+host=$(cat inventory | grep -Po "(.*)amazonaws\.com")
+
+echo "========== SSH command =========="
+echo "ssh -i $(pwd)/$cert_path -t ubuntu@$host"
+
+# output kommander and kaptain credentials
 if [ "$retrieve_credentials" == true ]; then
     echo '========== Retrieving credentials =========='
     ./retrieve-credentials.sh
