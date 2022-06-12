8cddb37003a871278e19f98ad9cb8191ab13e276
amoreno.c <amoreno.c@d2iq.com>
2022-06-12 14:55:37 -0400

    testing

--- install.bash
+++ install.bash
@@ -2,11 +2,11 @@
 set -euo pipefail
 #IFS=$'\n\t'
 
-apt-get update
+sudo apt-get update
 
 #apt-get install -y software-properties-common
 
-apt-get install -y \
+sudo apt-get install -y \
   curl \
   git \
   gnupg2 \
@@ -36,8 +36,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
   docker-ce \
   docker-ce-cli \
   containerd.io
